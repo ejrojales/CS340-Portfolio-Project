9a186b94fb6ed1acb437dd5984117d9a1e71d4db
EmmanuelJose Rojales <rojalese@flip1.engr.oregonstate.edu>
2022-10-25 03:37:47 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+/*
+    SETUP
+*/
+// Express
+var express = require('express');   // We are using the express library for the web server
+var app = express();            // We need to instantiate an express object to interact with the server in our code
+PORT = 8300;                 // Set a port number at the top so it's easy to change in the future
+
+// Database
+var db = require('./database/db-connector')
+
+// Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+/*
+    ROUTES
+*/
+app.get('/customers', function (req, res) {
+    res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+});                                         // will process this file, before sending the finished HTML to the client.
+
+app.get('/memberships', function (req, res) {
+    res.render('memberships');
+});
+
+app.get('/locations', function (req, res) {
+    res.render('locations');
+});
+
+app.get('/fitness_classes', function (req, res) {
+    res.render('fitness_classes');
+});
+
+app.get('/personal_trainers', function (req, res) {
+    res.render('personal_trainers');
+});
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function () {            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
