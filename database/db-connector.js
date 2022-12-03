ee40e318cb43ced0998b370e4ab5b5b30889693e
Emmanuel Jose Limtuatco Rojales <81449170+ejrojales@users.noreply.github.com>
2022-12-03 21:53:10 +1000

    Username and PW removed
    Project 6 guidelines stated username and password be removed from the dbcon file.

--- database/db-connector.js
+++ database/db-connector.js
@@ -9,8 +9,8 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_rojalese',
-    password: '8819',
+    user: '',
+    password: '',
     database: 'cs340_rojalese'
 })
 
